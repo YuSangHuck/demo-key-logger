faebddf9900948b27143323fabab9caf681474d6
yusanghuck <g01099387636@gmail.com>
2022-09-26 15:36:56 +0900

    map toString이 기본으로 formatting 이쁜거 확인

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        KeyLogger keyLogger = new KeyLogger();
-//        keyLogger = null;
-//        System.gc();
+        //        KeyLogger keyLogger = new KeyLogger();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(1, 0);
+        map.put(2, 0);
+        map.put(3, 0);
+        map.put(4, 0);
+        System.out.println(map);
     }
 }
