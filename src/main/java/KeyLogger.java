e450bd2c8062e42b2c87cb88ff36c99ae75b5778
yusanghuck <g01099387636@gmail.com>
2022-09-26 15:36:20 +0900

    key 입력 map에 저장

--- src/main/java/KeyLogger.java
+++ src/main/java/KeyLogger.java
@@ -2,10 +2,12 @@ import lc.kra.system.keyboard.GlobalKeyboardHook;
 import lc.kra.system.keyboard.event.GlobalKeyAdapter;
 import lc.kra.system.keyboard.event.GlobalKeyEvent;
 
-import java.util.Map;
+import java.util.HashMap;
 
 public class KeyLogger {
     private static boolean run = true;
+    //    입력 기록(순서 상관x)
+    private HashMap<Integer, Integer> map = new HashMap<>();
     private GlobalKeyboardHook keyboardHook;
 
     public KeyLogger() {
@@ -13,26 +15,17 @@ public class KeyLogger {
 //        버퍼? 버퍼쓸꺼면 죽기전에 버퍼에 있는거 File에 flush하고 close
         // Might throw a UnsatisfiedLinkError if the native library fails to load or a RuntimeException if hooking fails
         keyboardHook = new GlobalKeyboardHook(true); // Use false here to switch to hook instead of raw input
-
-        System.out.println("Global keyboard hook successfully started, press [escape] key to shutdown. Connected keyboards:");
-
-        for (Map.Entry<Long, String> keyboard : GlobalKeyboardHook.listKeyboards().entrySet()) {
-            System.out.format("%d: %s\n", keyboard.getKey(), keyboard.getValue());
-        }
-
         keyboardHook.addKeyListener(new GlobalKeyAdapter() {
 
             @Override
             public void keyPressed(GlobalKeyEvent event) {
-                System.out.println(event);
-                if (event.getVirtualKeyCode() == GlobalKeyEvent.VK_ESCAPE) {
-                    run = false;
-                }
             }
 
             @Override
             public void keyReleased(GlobalKeyEvent event) {
-                System.out.println(event);
+                int virtualKeyCode = event.getVirtualKeyCode();
+                int nextValue = map.getOrDefault(virtualKeyCode, 0) + 1;
+                map.put(virtualKeyCode, nextValue);
             }
         });
 
